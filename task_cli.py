491d120053ddde0639c1490465a2d6986105725e
Alvix.exe <alvixwick@gmail.com>
2025-05-04 22:24:38 -0400

    modify parsers for better performance and control

--- task_cli.py
+++ task_cli.py
@@ -4,17 +4,27 @@ from task_functions import task_add
 FILE_PATH = "tasks.json" # File name json
 
 # A parser is created to receive positional arguments.
-parser = argparse.ArgumentParser(description="Task Tracker")
-# An optional “--add” argument is defined that allows the user to add a new task from the command line.
-parser.add_argument("--add", type=str, help="Agregar una tarea nueva")
+parser = argparse.ArgumentParser(description="Task Tracker") 
+subparser = parser.add_subparsers(dest="command", help="Subcomandos disponibles")
+
+# Subcommand add
+add_parser = subparser.add_parser("add", help="Agrega una nueva tarea")
+add_parser.add_argument("description", type=str, help="Descripcion de la tarea")
+
+# Subcommand update
+add_parser = subparser.add_parser("update", help="Edita una tarea")
+add_parser.add_argument("id", type=int, help="Id de la tarea")
+add_parser.add_argument("description", type=str, help="Actualizar descripcion")
 
 # The arguments provided by the user from the command line are parsed.
 args = parser.parse_args()           
         
 def main():  
     '''Here you will control the flow of the CLI application.'''
-    if args.add:
+    if args.command == "add":
         task_add(args, FILE_PATH)
+    elif args.command == "update":
+        print("hola")
 
 if __name__ == "__main__":
     '''Run the application'''
