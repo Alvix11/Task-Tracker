fd27e57d7d9ae5dc964a40f7ea610f3c4c271f0a
Alvix.exe <alvixwick@gmail.com>
2025-05-08 12:29:49 -0400

    add a function to read json files

--- check_json.py
+++ check_json.py
@@ -1,7 +1,14 @@
 import os
+import json
 
 def verify_create_json(file_path):
     if not os.path.exists(file_path):
         return False # Return false if the json file does not exist
     else:
         return True # Returns true if the json file exists
+
+def read_json(file_path):
+    '''Function to read json files'''
+    with open(file_path, "r") as file:
+        datas = json.load(file)
+    return datas
