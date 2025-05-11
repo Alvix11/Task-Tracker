d26f64a5f200406b8ab9e7fbbe0461d4ac9e45a9
Alvix.exe <alvixwick@gmail.com>
2025-05-10 23:35:01 -0400

    Simplify calls on parameters by creating a better variable

--- task_cli.py
+++ task_cli.py
@@ -2,21 +2,22 @@ from task_functions import TaskManager
 from settings import Parser
 
 args = Parser.parser_settings()
+file_path = Parser.FILE_PATH
 
 def main():  
     '''Here you will control the flow of the CLI application.'''
     if args.command == "add":
-        TaskManager.task_add(args, Parser.FILE_PATH)
+        TaskManager.task_add(args, file_path)
     elif args.command == "update":
-        TaskManager.task_update(args, Parser.FILE_PATH)
+        TaskManager.task_update(args, file_path)
     elif args.command == "delete":
-        TaskManager.task_delete(args, Parser.FILE_PATH)
+        TaskManager.task_delete(args, file_path)
     elif args.command == "mark-in-progress":
-        TaskManager.task_mark_in_progress(args, Parser.FILE_PATH)
+        TaskManager.task_mark_in_progress(args, file_path)
     elif args.command  == "mark-done":
-        TaskManager.task_mark_done(args, Parser.FILE_PATH)
+        TaskManager.task_mark_done(args, file_path)
     elif args.command == "list":
-        TaskManager.tasks_list(args, Parser.FILE_PATH)
+        TaskManager.tasks_list(args, file_path)
             
 
 if __name__ == "__main__":
