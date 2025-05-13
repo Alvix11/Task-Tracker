83f01d675be4776e7c6b14f242c6093e8abd2f2d
Alvix.exe <alvixwick@gmail.com>
2025-05-12 23:30:41 -0400

    I modified it to adapt to the update of the task_functions.py file.

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 from task_functions import TaskManager
-from settings import Parser
+from settings import get_parser
 
-args = Parser.parser_settings()
-file_path = Parser.FILE_PATH
+args = get_parser()
 
 def main():  
     '''Here you will control the flow of the CLI application.'''
+    manager = TaskManager()
+
     if args.command == "add":
-        TaskManager.task_add(args, file_path)
+        manager.task_add(args)
     elif args.command == "update":
-        TaskManager.task_update(args, file_path)
+        manager.task_update(args)
     elif args.command == "delete":
-        TaskManager.task_delete(args, file_path)
+        manager.task_delete(args)
     elif args.command == "mark-in-progress":
-        TaskManager.task_mark_in_progress(args, file_path)
+        manager.task_mark_in_progress(args)
     elif args.command  == "mark-done":
-        TaskManager.task_mark_done(args, file_path)
+        manager.task_mark_done(args)
     elif args.command == "list":
-        TaskManager.tasks_list(args, file_path)       
+        manager.tasks_list(args)       
 
 if __name__ == "__main__":
     '''Run the application'''
