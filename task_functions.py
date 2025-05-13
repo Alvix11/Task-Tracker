41082b0f531e6cf219068744635c6d4fac04e7aa
Alvix.exe <alvixwick@gmail.com>
2025-05-12 23:28:58 -0400

    Refactorize the code to make the main cleaner and write less code.

--- task_functions.py
+++ task_functions.py
@@ -1,15 +1,18 @@
 from utils import actual_date_and_time
 from helpers import load_tasks, save_tasks, display_tasks, is_text
+from settings import FILE_PATH
 
-class TaskManager():
+class TaskManager:
 
-    def task_add(args, file_path):
+    file_path = FILE_PATH
+
+    def task_add(self, args):
         '''Function to add new tasks'''
         datas = {} # Dictionary to be used if the file does not exist
         id = 1 # Id to be used if no tasks are created
         present_date = actual_date_and_time() # We obtain the current date and time
 
-        data = load_tasks(file_path)
+        data = load_tasks(self.file_path)
 
         if data: # We verify that there are already saved tasks to get the last id and increment it.
             id = max(map(int, data.keys())) + 1
@@ -22,7 +25,7 @@ class TaskManager():
                     "createdAt": str(present_date),
                     "updateAt": "",
                 }
-                save_tasks(file_path, data)
+                save_tasks(self.file_path, data)
                 print(f"Tarea agregada con el id: {id}")
         else:
             if is_text(args):
@@ -33,68 +36,68 @@ class TaskManager():
                     "createdAt": str(present_date),
                     "updateAt": "",
                 }
-                save_tasks(file_path, datas)
+                save_tasks(self.file_path, datas)
                 print(f"Tarea creada con el id: {id}")
 
-    def task_update(args, file_path):
+    def task_update(self, args):
         '''Function to update tasks'''
         present_date = actual_date_and_time()
 
-        data = load_tasks(file_path)
+        data = load_tasks(self.file_path)
         if data:
             if str(args.id) in data: # We verify that the id exists
                 if is_text(args):
                     data[str(args.id)]["description"] = str(args.description) # Update description
                     data[str(args.id)]["updateAt"] = str(present_date) # Update updateAt
-                    save_tasks(file_path, data)
+                    save_tasks(self.file_path, data)
                     print(f"Tarea con el id: {args.id} actualizada") 
             else:
                 print(f"La tarea con el id: {args.id} no existe")
         else:
             print("No existen tareas para actualizar, crea una tarea primero con el comando add")
 
-    def task_delete(args, file_path):
+    def task_delete(self, args):
         '''Funtion to delete task'''
-        data = load_tasks(file_path)
+        data = load_tasks(self.file_path)
 
         if data:
             if str(args.id) in data: 
                 del data[str(args.id)] # Delete task
-                save_tasks(file_path, data) 
+                save_tasks(self.file_path, data) 
                 print(f"Tarea con el id: {args.id} eliminada")
             else:
                 print(f"La tarea con el id: {args.id} no existe")
         else:
                 print("No existen tareas, crea una tarea primero con el comando add")
 
-    def task_mark_in_progress(args, file_path):
+    def task_mark_in_progress(self, args):
         '''Function to mark tasks in progress'''
         present_date = actual_date_and_time()
 
-        data = load_tasks(file_path)
+        data = load_tasks(self.file_path)
 
         if data:
             if str(args.id) in data: 
                 data[str(args.id)]["status"] = "in-progress"
                 data[str(args.id)]["updateAt"] = str(present_date)
-                save_tasks(file_path, data)
+                save_tasks(self.file_path, data)
                 print(f"Tarea con el id: {args.id} marcada en progreso")
             else:
                 print(f"La tarea con el id: {args.id} no existe")
         else:
             print("No existen tareas, crea una tarea primero con el comando add")
 
-    def task_mark_done(args, file_path):
+    def task_mark_done(self, args):
         '''Function to mark tasks in progress'''
         present_date = actual_date_and_time()
 
-        data = load_tasks(file_path)
+        data = load_tasks(self.file_path)
 
         if data:
             if str(args.id) in data:
                 data[str(args.id)]["status"] = "done" 
                 data[str(args.id)]["updateAt"] = str(present_date)
-                save_tasks(file_path, data)
+                save_tasks(self.file_path, data)
                 print(f"Tarea con el id: {args.id} marcada como hecha")
             else:
                 print(f"La tarea con el id: {args.id} no existe")
@@ -102,9 +105,9 @@ class TaskManager():
             print("No existen tareas, crea una tarea primero con el comando add")
 
 
-    def tasks_list(args, file_path):
+    def tasks_list(self, args):
         '''Function to list all tasks'''
-        data = load_tasks(file_path)
+        data = load_tasks(self.file_path)
         if data:
             if args.status:
                 filtered = {k: v for k, v in data.items() if v["status"] == args.status}
