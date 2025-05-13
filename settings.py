d48edb6c4fdb1d3a0e346dbd46b23075924d7f46
Alvix.exe <alvixwick@gmail.com>
2025-05-12 23:05:00 -0400

    Improve readability and remove the Parser class as it was not needed.

--- settings.py
+++ settings.py
@@ -1,46 +1,44 @@
 import argparse
 
-class Parser():
+FILE_PATH = "tasks.json" # File name json
 
-    FILE_PATH = "tasks.json" # File name json
+def get_parser():
+    '''Function to get the user's entries on the console'''
 
-    def parser_settings():
+    # A parser is created to receive positional arguments.
+    parser = argparse.ArgumentParser(description="Task Tracker") 
+    subparser = parser.add_subparsers(dest="command", help="Available subcommands")
 
-        # A parser is created to receive positional arguments.
-        parser = argparse.ArgumentParser(description="Task Tracker") 
-        subparser = parser.add_subparsers(dest="command", help="Available subcommands")
+    # Subcommand add
+    add = subparser.add_parser("add", help="Add a new task")
+    add.add_argument("description", type=str, help="Task description")
 
-        # Subcommand add
-        add_parser = subparser.add_parser("add", help="Add a new task")
-        add_parser.add_argument("description", type=str, help="Task description")
+    # Subcommand update
+    update = subparser.add_parser("update", help="Edit a task")
+    update.add_argument("id", type=int, help="Task id")
+    update.add_argument("description", type=str, help="Update description")
 
-        # Subcommand update
-        add_parser = subparser.add_parser("update", help="Edit a task")
-        add_parser.add_argument("id", type=int, help="Task id")
-        add_parser.add_argument("description", type=str, help="Update description")
+    # Subcommand delete
+    delete = subparser.add_parser("delete", help="Task delete")
+    delete.add_argument("id", type=int, help="Task id")
 
-        # Subcommand delete
-        add_parser = subparser.add_parser("delete", help="Task delete")
-        add_parser.add_argument("id", type=int, help="Task id")
+    # Subcommand mark-in-progress
+    in_progress = subparser.add_parser("mark-in-progress", help="Mark a task as in progress")
+    in_progress.add_argument("id", type=int, help="Task id")
 
-        # Subcommand mark-in-progress
-        add_parser = subparser.add_parser("mark-in-progress", help="Mark a task as in progress")
-        add_parser.add_argument("id", type=int, help="Task id")
+    # Subcommand mark-done
+    done = subparser.add_parser("mark-done", help="Mark a task as done")
+    done.add_argument("id", type=int, help="Task id")
 
-        # Subcommand mark-done
-        add_parser = subparser.add_parser("mark-done", help="Mark a task as done")
-        add_parser.add_argument("id", type=int, help="Task id")
-
-        # Subcommand list all tasks
-        add_parser = subparser.add_parser("list", help="List all task")
-        add_parser.add_argument(
-                                "status", 
-                                nargs="?", 
-                                choices=["done", "todo", "in-progress"],
-                                default=None, 
-                                help="Filter tasks"
-                                )
-
-        # Returns user-supplied arguments from the command line
-        return parser.parse_args()           
-        
+    # Subcommand list all tasks
+    list_cmd = subparser.add_parser("list", help="List all task")
+    list_cmd.add_argument(
+                            "status", 
+                            nargs="?", 
+                            choices=["done", "todo", "in-progress"],
+                            default=None, 
+                            help="Filter tasks"
+                            )
+    
+    # Returns user-supplied arguments from the command line
+    return parser.parse_args() 
